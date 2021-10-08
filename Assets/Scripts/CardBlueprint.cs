730760940e32ac766c2d034fc020de7eede3754b
unknown <superweasel3@gmail.com>
2021-10-07 17:32:10 -0700

    changed text with code

---
+++ Assets/Scripts/CardBlueprint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBlueprint
+{
+    string name;
+    string flavor;
+    int base_cost;
+    int current_cost;
+    int element;
+    int type;
+    int scendance;
+}
---
+++ Assets/Scripts/CardEquipper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardEquipper : MonoBehaviour
+{
+    private int[] equipment = new int[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        equipment[0] = 5;
+        print(gameObject.transform.GetChild(0).GetChild(0).GetComponent<Text>().text) ;
+        gameObject.transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "ahh";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
